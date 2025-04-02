95c6883e25eb5abb24c239262bbda33b97cf61c6
Folylolyboyz <basak.nabajyoti.play@gmail.com>
2025-04-02 06:17:32 +0530

    Updated vite.config.js to allow all hosts

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: ["indian-sign-language-detection-production.up.railway.app"],
+    allowedHosts: true,
   },
 });
